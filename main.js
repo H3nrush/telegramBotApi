8c2f3248769b6203bcf0b891bf82db9ed19139aa
Hassan <hassanmohammadi078@gmail.com>
2026-01-03 17:32:32 +0100

    update

--- main.js
+++ main.js
@@ -6,7 +6,16 @@ const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
   console.log(msg);
-  bot.sendMessage(msg.chat.id, 'Hello ' + msg.chat.first_name + '! ' + 'Bot is running for You🚀');
+  bot.sendMessage(msg.chat.id, 'Hello ' + msg.chat.first_name + '! ' + 'Bot is running for You🚀' ,
+    {
+        reply_markup : {
+            'keyboard' :[
+                            ['Menu','About'],
+                            ['Close Bot']
+        ]
+        }
+    }
+  );
 });
 
 bot.on('message' , msg=>{
@@ -14,6 +23,19 @@ bot.on('message' , msg=>{
     if(msg.text == 'Hello'){
         bot.sendPhoto(msg.chat.id , 'AgACAgQAAxkBAAMbaVlAGXcfTd4_dfnE5o5bf_6c0rcAAkcMaxvpSchSzkx212zOFXoBAAMCAAN4AAM4BA')
     }
+
+    switch(msg.text){
+        case 'Menu' :
+            bot.sendMessage(msg.chat.id , 't.me/mikkarra')
+            break;
+        case 'About' :
+            bot.sendMessage(msg.chat.id , 'this robot is created by @Mikkarra')
+            break;
+        case 'Close Bot' :
+            bot.sendMessage(msg.chat.id , 'you are trying to close the bot') 
+            break;
+    }
+
 })
 
 console.log('bot is running...');
